b276b61cc66749f448b479a8764c61651bf894d3
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2022-04-13 17:33:04 -0300

    feat: list products

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,8 @@
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-import { ServerStyleSheets } from '@material-ui/core';
-import Document, {Head, Html, Main, NextScript} from 'next/document';
 
-export default class MyDocumnet extends Document{
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
@@ -12,27 +12,24 @@ export default class MyDocumnet extends Document{
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-MyDocumnet.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async (ctx) => {
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
-
   ctx.renderPage = () => {
     return originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
   };
-
   const initialProps = await Document.getInitialProps(ctx);
-
   return {
-    ...initialProps, 
+    ...initialProps,
     styles: [
       ...React.Children.toArray(initialProps.styles),
       sheets.getStyleElement(),
-    ]
-  }
-}
+    ],
+  };
+};
